cddce88ba33c6326cddfe684668db7c2db5aa5d6
Dmitry Maksimov <dmtmax@gmail.com>
2019-10-18 22:15:04 +0200

    remov balance command from main package

--- printer.go
+++ printer.go
@@ -10,28 +10,6 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-func printReport(r report) {
-	// Sort accounts
-	accounts := []string{}
-	for k := range r {
-		accounts = append(accounts, k)
-	}
-	sort.Strings(accounts)
-
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-
-	for _, account := range accounts {
-		ri := r[account]
-		if !ri.total.Equal(decimal.Zero) {
-			fmt.Fprintf(w, "%s\t%s\n", ri.account.name, ri.total.StringFixed(2))
-		}
-	}
-	fmt.Fprintln(w, "-----\t")
-	fmt.Fprintf(w, "Total\t%s\n", r.total().StringFixed(2))
-
-	w.Flush()
-}
-
 func printWeeklyReport(r *weeklyReport) {
 	// Sort assets
 	assets := make([]string, len(r.assets))
