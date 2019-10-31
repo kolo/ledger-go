d14db70ed8304c8bddac80c0cd497caf5c955c75
Dmitry Maksimov <dmtmax@gmail.com>
2019-10-31 11:54:22 +0100

    cleanup

--- main.go
+++ main.go
@@ -7,36 +7,10 @@ import (
 	"github.com/kolo/ledger-go/cmd"
 )
 
-const (
-	configFilename  = "config.json"
-	ledgerDirEnvKey = "LEDGER_DIR"
-)
-
 func main() {
 	if err := cmd.Execute(); err != nil {
 		exitWithError(err)
 	}
-
-	// env, err := loadEnvironment()
-	// if err != nil {
-	// 	exitWithErr(err)
-	// }
-
-	// cmd := &cobra.Command{
-	// 	Use: "ledger",
-	// 	Run: func(cmd *cobra.Command, args []string) {
-	// 		cmd.Help()
-	// 	},
-	// }
-
-	// cmd.AddCommand(newExpensesCommand(env))
-	// cmd.AddCommand(newBalanceCommand(env))
-	// cmd.AddCommand(newExportCommand(env))
-	// cmd.AddCommand(newLogCommand(env))
-
-	// if err := cmd.Execute(); err != nil {
-	// 	exitWithErr(err)
-	// }
 }
 
 func exitWithError(err error) {
