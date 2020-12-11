1c2b44d922d3aba360ae37a908af176390b580c0
Dmitry Maksimov <dmtmax@gmail.com>
2020-12-11 16:18:47 +0100

    export to ldg file

--- cmd/export.go
+++ cmd/export.go
@@ -3,8 +3,10 @@ package cmd
 import (
 	"encoding/csv"
 	"errors"
+	"fmt"
 	"io"
 	"os"
+	"time"
 
 	"github.com/kolo/ledger-go/ledger"
 	"github.com/spf13/cobra"
@@ -31,12 +33,12 @@ func newExportCmd() *cobra.Command {
 
 	export.addFlags(cmd.Flags())
 
-	export.run = func(_ *ledger.Config, iter ledger.RecordIterator, stdout io.Writer) error {
+	export.run = func(config *ledger.Config, iter ledger.RecordIterator, stdout io.Writer) error {
 		switch export.format {
 		case "csv":
 			return exportToCSV(iter, stdout)
-		case "sqlite":
-			return exportToSQLite(iter, stdout)
+		case "ldg":
+			return exportToLdg(config, iter, stdout)
 		default:
 			return errors.New("unknown export format")
 		}
@@ -74,6 +76,63 @@ func exportToCSV(iter ledger.RecordIterator, output io.Writer) error {
 	return nil
 }
 
-func exportToSQLite(iter ledger.RecordIterator, output io.Writer) error {
+func exportToLdg(config *ledger.Config, iter ledger.RecordIterator, stdout io.Writer) error {
+	f, err := os.OpenFile("journal.ldg", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	var lastDate time.Time
+
+	flushPairing := func(p []*ledger.Record) {
+		if len(p) == 0 {
+			return
+		}
+		if len(p) == 1 {
+			f.WriteString(fmt.Sprintf("  %s,%s,%s\n", p[0].Credit.Name, p[0].Debit.Name, p[0].Amount.StringFixed(2)))
+			return
+		}
+		f.WriteString(fmt.Sprintf("  %s {\n", p[0].Credit.Name))
+		for _, pp := range p {
+			f.WriteString(fmt.Sprintf("    %s,%s\n", pp.Debit.Name, pp.Amount.StringFixed(2)))
+		}
+		f.WriteString("  }\n")
+	}
+
+	flushRecord := func(r []*ledger.Record) {
+		if len(r) == 0 {
+			return
+		}
+		f.WriteString(fmt.Sprintf("%s {\n", r[0].Date.Format("2006-01-02")))
+		var i int
+		var lastCredit string
+		for j, rr := range r {
+			if rr.Credit.Name != lastCredit {
+				flushPairing(r[i:j])
+				lastCredit = rr.Credit.Name
+				i = j
+			}
+		}
+		flushPairing(r[i:])
+		f.WriteString("}\n")
+	}
+
+	record := []*ledger.Record{}
+	for {
+		r := iter.Next()
+		if r == nil {
+			break
+		}
+
+		if !r.Date.Equal(lastDate) {
+			lastDate = r.Date
+			flushRecord(record)
+			record = record[:0]
+			record = append(record, r)
+		} else {
+			record = append(record, r)
+		}
+	}
 	return nil
 }
